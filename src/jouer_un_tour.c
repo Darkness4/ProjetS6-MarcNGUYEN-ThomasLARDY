7b1fc602d78ec3cc1758027f925ee96c1173d1bf
Nguyen Marc <nguyen_marc@live.fr>
2019-03-28 15:03:42 +0100

    Doc; better usage

--- src/jouer_un_tour.c
+++ src/jouer_un_tour.c
@@ -1,3 +1,20 @@
+/**
+ * @file jouer_un_tour.c
+ *
+ * @brief Lancer un tour.
+ *
+ * Fichier principal.
+ *
+ * Fonctionnalités :
+ * - Détecteur de zombie.
+ * - jouerTour lancer un tour.
+ *
+ * @author Marc NGUYEN
+ * @author Thomas LARDY
+ * @date 28 Mar 2019
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,16 +24,21 @@
 /**
  * @brief Renvoie true s'il y a encore au moins un zombie dans la population, false dans le cas contraire.
  * 
+ * Usage:
+ * @code
+ * struct Population* population = creerPopulation(7);
+ * char boolean = presenceZombie(population);  // -> false;
+ * @endcode
+ * 
  * @param population Grille représentant la population.
  * @return char Booléen, si zombies toujours présents.
  */
 char presenceZombie(struct Population *population)
 {
-	int i, j;
-	long cote = population->cote;
-	for (i = 0; i < cote; i++)
+	unsigned long cote = population->cote;
+	for (unsigned long i = 0; i < cote; i++)
 	{
-		for (j = 0; j < cote; j++)
+		for (unsigned long j = 0; j < cote; j++)
 		{
 			if (population->grille_de_personnes[i][j]->state == MALADE)
 				return 1;
@@ -25,17 +47,26 @@ char presenceZombie(struct Population *population)
 	return 0;
 }
 
-
 /**
  * @brief Faire passer la grille du temps t au temps t+1.
+ * 
+ * Usage pour jouer un jeu complet:
+ * @code
+ * char est_fini = 0;
+ * int tour_max = 500;
+ * struct Population* population = creerPopulation(7);  // TODO add function infecter.
+ * for(int i = 0; (i<tour_max)&&(!est_fini); i++) {
+ *     jouerTour(population);
+ *     est_fini = presenceZombie(population);
+ * }
+ * @endcode
  * 
  * @param population Grille au temps t.	
  * @param beta Probabilité d'un individu infecté de mourir.
  * @param gamma Probabilité d'un individu infecté de guérir.
  * @param lambda Probabilité d'un individu sain d'être contaminé par un individu voisin infecté.
- * @return char Retourne si la population est maintenant vide.
  */
-char jouerTour(struct Population *population, double beta, double gamma, double lambda)
+void jouerTour(struct Population *population, double beta, double gamma, double lambda)
 {
 	const long unsigned cote = population->cote;
 
@@ -72,9 +103,9 @@ char jouerTour(struct Population *population, double beta, double gamma, double 
 				// (a == a) = 1
 				// (a != a) = 0
 				voisin += (j > 0) * (grille[i][j - 1]->state == MALADE) +		 // j [1:n]
-							(j < cote - 1) * (grille[i][j + 1]->state == MALADE) + // j [0:n-1]
-							(i > 0) * (grille[i - 1][j]->state == MALADE) +		 // i [1:n]
-							(i < cote - 1) * (grille[i + 1][j]->state == MALADE);  // i [0:n-1]
+						  (j < cote - 1) * (grille[i][j + 1]->state == MALADE) + // j [0:n-1]
+						  (i > 0) * (grille[i - 1][j]->state == MALADE) +		 // i [1:n]
+						  (i < cote - 1) * (grille[i + 1][j]->state == MALADE);  // i [0:n-1]
 				//on applique la probabilité que l'individu tombe malade pour chaque voisin.
 				while (voisin != 0 && grille_tampon[i][j]->state != MALADE)
 				{
@@ -91,9 +122,4 @@ char jouerTour(struct Population *population, double beta, double gamma, double 
 	}
 	free(population);				// Supprime l'original
 	population = population_tampon; // On met à jour la grille.
-	// S'il n'y a plus de zombie le programme doit arrêter de jouer des tours.
-	if (!presenceZombie(population))
-		return 1; 
-	else
-		return 0;
 }
